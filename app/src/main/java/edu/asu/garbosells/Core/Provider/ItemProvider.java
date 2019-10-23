533041cb424c59cdf06ffe448b74ee6e3176a0d0
thatboramy <mcboramy@gmail.com>
2019-10-23 00:28:08 -0700

    Started ItemProvider

--- app/src/main/java/edu/asu/garbosells/Core/Provider/ItemProvider.java
+++ app/src/main/java/edu/asu/garbosells/Core/Provider/ItemProvider.java
@@ -1,4 +1,24 @@
 package edu.asu.garbosells.Core.Provider;
+import java.util.ArrayList;
+
+import edu.asu.garbosells.Core.Provider.Item.Item;
+import edu.asu.garbosells.Core.Provider.Item.GeneralItemAttributes;
 
 public class ItemProvider {
+    ArrayList<Item> temp_items;
+
+    public ArrayList<Item> getMockItems(){
+        temp_items = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            Item temp_item = new Item();
+            temp_item.shortDescription = "TestItem "+ i;
+            temp_item.categoryId = i %2;
+
+            GeneralItemAttributes temp_GA = new GeneralItemAttributes();
+            temp_item.generalItemAttributes = temp_GA;
+            temp_items.add(temp_item);
+        }
+
+        return temp_items;
+    }
 }
