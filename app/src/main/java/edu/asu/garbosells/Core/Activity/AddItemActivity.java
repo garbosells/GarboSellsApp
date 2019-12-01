7f1ba13e5c490e9a0cc8fbfd75f2f915686b3b58
Erica Anderson <anderson.erica.lee@gmail.com>
2019-11-30 22:24:58 -0700

    add nav drawer to add item activity

--- app/src/main/java/edu/asu/garbosells/Core/Activity/AddItemActivity.java
+++ app/src/main/java/edu/asu/garbosells/Core/Activity/AddItemActivity.java
@@ -1,15 +1,127 @@
 package edu.asu.garbosells.Core.Activity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 
 import edu.asu.garbosells.R;
+import edu.asu.garbosells.UserManagement.AppHelper;
+import edu.asu.garbosells.UserManagement.SettingsActivity;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    private NavigationView nDrawer;
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String username;
+    private CognitoUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Set toolbar for this screen
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar.setTitle("");
+        TextView main_title = (TextView) findViewById(R.id.main_toolbar_title);
+        main_title.setText("Listings");
+        setSupportActionBar(toolbar);
+
+        // Set navigation drawer for this screen
+        mDrawer = (DrawerLayout) findViewById(R.id.add_item_drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        mDrawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        nDrawer = (NavigationView) findViewById(R.id.nav_view);
+        setNavDrawer();
+        init();
+        View navigationHeader = nDrawer.getHeaderView(0);
+        TextView navHeaderSubTitle = (TextView) navigationHeader.findViewById(R.id.textViewNavUserSub);
+        navHeaderSubTitle.setText(username);
+    }
+
+    // Handle when the a navigation item is selected
+    private void setNavDrawer() {
+        nDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                performAction(item);
+                return true;
+            }
+        });
+    }
+
+    // Initialize this activity
+    private void init() {
+        // Get the user name
+        username = AppHelper.getCurrUser();
+        user = AppHelper.getPool().getUser(username);
+    }
+
+    // Perform the action for the selected navigation item
+    private void performAction(MenuItem item) {
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+
+        // Find which item was selected
+        switch(item.getItemId()) {
+            case R.id.nav_item_add_item:
+                goToAddItemActivity();
+                break;
+            case R.id.nav_item_view_list:
+                goToListActivity();
+                break;
+            case R.id.nav_item_settings:
+                goToSettingsActivity();
+                break;
+            case R.id.nav_user_sign_out:
+                // Sign out from this account
+                signOut();
+                break;
+        }
+    }
+
+    private void goToAddItemActivity() {
+        Intent intent = new Intent(this, AddItemActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToListActivity() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    // Sign out user
+    private void signOut() {
+        user.signOut();
+        exit();
+    }
+
+    private void exit () {
+        Intent intent = new Intent();
+        if(username == null)
+            username = "";
+        intent.putExtra("name",username);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
