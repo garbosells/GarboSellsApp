ebc877b0a6067f94cc0a798a26ebb0e62e29ec53
Erica Anderson <anderson.erica.lee@gmail.com>
2019-12-03 12:20:46 -0700

    set description input listener

--- app/src/main/java/edu/asu/garbosells/Core/Activity/AddItem/InputWizardActivity.java
+++ app/src/main/java/edu/asu/garbosells/Core/Activity/AddItem/InputWizardActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
@@ -17,17 +24,23 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.asu.garbosells.API.Providers.TemplateProvider;
 import edu.asu.garbosells.Core.Activity.ListActivity;
+import edu.asu.garbosells.Item.Item;
 import edu.asu.garbosells.R;
+import edu.asu.garbosells.Template.Recommendation;
+import edu.asu.garbosells.Template.Size;
 import edu.asu.garbosells.Template.Subcategory;
 import edu.asu.garbosells.Template.Template;
 import edu.asu.garbosells.UserManagement.AppHelper;
 import edu.asu.garbosells.UserManagement.SettingsActivity;
 
-public class InputWizardActivity extends AppCompatActivity {
+import static java.security.AccessController.getContext;
+
+public class InputWizardActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private NavigationView nDrawer;
     private DrawerLayout mDrawer;
@@ -37,6 +50,18 @@ public class InputWizardActivity extends AppCompatActivity {
     private CognitoUser user;
     private Subcategory subcategory;
     private Template template;
+
+    EditText editTextDescription;
+
+    Spinner sizeTypeSpinner;
+    Spinner sizeValueSpinner;
+
+    long selectedSizeTypeId = -1;
+    long selectedSizeValueId = -1;
+    View sizeValueContainer;
+
+    private Item item;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +95,94 @@ public class InputWizardActivity extends AppCompatActivity {
         View navigationHeader = nDrawer.getHeaderView(0);
         TextView navHeaderSubTitle = (TextView) navigationHeader.findViewById(R.id.textViewNavUserSub);
         navHeaderSubTitle.setText(username);
+
+        item = new Item();
+        setupInputForm();
+    }
+
+    private void setupInputForm() {
+        editTextDescription = findViewById(R.id.edittext_short_description);
+        editTextDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                updateShortDescription(editTextDescription.getText().toString().trim());
+            }
+        });
+
+        int step = 2;
+        if(template.category.hasSizing) {
+            setupSizeInput(step);
+        }
+    }
+
+    private void updateShortDescription(String text) {
+        item.setShortDescription(text);
+    }
+
+    private void setupSizeInput(int step) {
+        sizeValueContainer = findViewById(R.id.item_size_value_input_container);
+        TextView textViewStep = findViewById(R.id.textview_size_step_number);
+        textViewStep.setText(String.valueOf(step));
+
+        View view = findViewById(R.id.input_size_layout);
+        view.setVisibility(View.VISIBLE);
+        sizeTypeSpinner = findViewById(R.id.spinner_size_type);
+        sizeValueSpinner = findViewById(R.id.spinner_size_value);
+        sizeValueSpinner.setEnabled(false);
+
+        //SET UP ADAPTER
+        ArrayList<Size> options = (ArrayList<Size>) template.sizeOptions;
+        Size nullOption = new Size();
+        nullOption.sizeTypeId = -1;
+        nullOption.sizeTypeDescription = "Select a Scale";
+        options.add(0, nullOption);
+        ArrayAdapter<Size> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeTypeSpinner.setAdapter(arrayAdapter);
+        //SET SPINNER ON ITEM SELECTED LISTENER
+        sizeTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Size selection = (Size) adapterView.getSelectedItem();
+                if(selection.sizeTypeId == 3) {
+                    //one-size
+                    sizeValueContainer.setVisibility(View.INVISIBLE);
+                }
+                else if(selection.sizeTypeId >= 0) {
+                    List<Recommendation> options = selection.recommendations;
+                    ArrayAdapter<Recommendation> sizeValueAdapter = new ArrayAdapter<>(InputWizardActivity.this, android.R.layout.simple_spinner_item, options);
+                    sizeValueAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sizeValueSpinner.setAdapter(sizeValueAdapter);
+                    sizeValueSpinner.setEnabled(true);
+                    sizeValueContainer.setVisibility(View.VISIBLE);
+                } else {
+                    Recommendation nullRecommendation = new Recommendation();
+                    nullRecommendation.id = -1;
+                    nullRecommendation.description = "Choose a scale";
+                    ArrayAdapter<Recommendation> emptySizeValueAdapter = new ArrayAdapter<>(InputWizardActivity.this, android.R.layout.simple_spinner_item, new Recommendation[] {nullRecommendation});
+                    emptySizeValueAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sizeValueSpinner.setAdapter(emptySizeValueAdapter);
+                    sizeValueSpinner.setEnabled(false);
+                    sizeValueContainer.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
     // Handle when the a navigation item is selected
@@ -141,5 +254,15 @@ public class InputWizardActivity extends AppCompatActivity {
         intent.putExtra("name", username);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
