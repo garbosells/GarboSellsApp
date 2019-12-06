6a61b2621364d4326b445461a7237fdd0fd657aa
Erica Anderson <anderson.erica.lee@gmail.com>
2019-12-05 23:01:16 -0700

    admin check not working, hardcoding temp solution

--- app/src/main/java/edu/asu/garbosells/Core/Activity/AddItem/InputWizardActivity.java
+++ app/src/main/java/edu/asu/garbosells/Core/Activity/AddItem/InputWizardActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -498,9 +499,9 @@ public class InputWizardActivity extends AppCompatActivity implements AdapterVie
     }
 
     public boolean userCanPost() {
-        String userLoginId = user.getUserId();
-        OkHttpClient client = new OkHttpClient();
-        String url = "https://listingservice.azurewebsites.net/api/Listing/PostListing";
+        return AppHelper.getCurrUser().equals("admin");
+/*        OkHttpClient client = new OkHttpClient();
+        String url = "https://garbosellsuserdataservice.azurewebsites.net/api/user/GetUserByUserLoginId?userLoginId=" + userLoginId;
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("Connection","close")
@@ -514,7 +515,7 @@ public class InputWizardActivity extends AppCompatActivity implements AdapterVie
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return false;
+        return false;*/
     }
 
     @Override
@@ -593,7 +594,36 @@ public class InputWizardActivity extends AppCompatActivity implements AdapterVie
             Type PostResponseType = new TypeToken<PostResponse>() {
             }.getType();
             PostResponse response = new Gson().fromJson(result, PostResponseType);
-            alertDialogBuilder.setMessage(result);
+            PostResponse.SiteResponse testResponse = response.postEbayListingResponse;
+            String test = "isSUCC: " + Boolean.toString(testResponse.isSuccess) + "\nListingId: " + Long.toString(testResponse.listingId);
+
+            Log.d("debug","PRINT" + testResponse.getlistingId());
+
+            String ebayString = "";
+            String etsyString = "";
+            //Ebay Stuff
+            if(postListingRequest.postToEbay == true){
+                PostResponse.SiteResponse EbayResponse = response.postEbayListingResponse;
+                if(EbayResponse.isSuccess == true){
+                    ebayString = "Posted to Ebay!\nListingId: " + EbayResponse.getlistingId();
+                }
+                else{
+                    ebayString = "Attempted to post to Ebay, but Failed.\nError Message: " + EbayResponse.errorMessage;
+                }
+            }else{ebayString = "Did not post to EBay.";}
+
+            //Etsy Stuff
+            if(postListingRequest.postToEtsy == true){
+                PostResponse.SiteResponse EtsyResponse = response.postEtsyListingResponse;
+                if(EtsyResponse.isSuccess == true){
+                    etsyString = "Posted to Etsy!\nListingId: " + EtsyResponse.getlistingId();
+                }else{
+                    etsyString = "Attempted to post to Etsy, but Failed.\nError Message: " + EtsyResponse.errorMessage;
+                }
+            }else{etsyString = "Did not post to Etsy.";}
+
+
+            alertDialogBuilder.setMessage("EBAY:\n" + ebayString+ "\n\nETSY:\n" + etsyString);
             alertDialogBuilder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
